a45b8c136615b1deff3f66a0b0b9909f4df34248
Movsar Makhmutov <m.mahmutov@urbi.ae>
2025-01-26 22:50:22 +0300

    fix deploy

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,10 +95,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static') 
+STATIC_ROOT = '/app/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
