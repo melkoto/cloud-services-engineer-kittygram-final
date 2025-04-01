78b8e8d81d68af04b6d665263ac095c9abaf1311
Movsar Makhmutov <m.mahmutov@urbi.ae>
2025-04-01 19:37:32 +0300

    Update VM configuration and streamline provisioning steps
    Enabled `allow_stopping_for_update` to facilitate VM updates. Replaced comments in provisioning scripts for clarity and made minor adjustments to Docker installation steps, ensuring consistency and improved readability.

--- infra/compute.tf
+++ infra/compute.tf
@@ -2,6 +2,8 @@ resource "yandex_compute_instance" "kittygram_vm" {
   name        = "kittygram-vm"
   platform_id = "standard-v3"
 
+  allow_stopping_for_update = true
+
   resources {
     cores  = 2
     memory = 2
@@ -21,7 +23,7 @@ resource "yandex_compute_instance" "kittygram_vm" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file(var.ssh_pub_key_path)}"
+    ssh-keys  = "ubuntu:${file(var.ssh_pub_key_path)}"
   }
 
   connection {
@@ -34,31 +36,28 @@ resource "yandex_compute_instance" "kittygram_vm" {
 
   provisioner "remote-exec" {
     inline = [
-      // Обновляем пакеты и устанавливаем утилиты с принудительным использованием IPv4
+      # Обновляем пакеты и устанавливаем необходимые утилиты
       "sudo apt-get update -o Acquire::ForceIPv4=true",
       "sudo apt-get install -y -o Acquire::ForceIPv4=true apt-transport-https ca-certificates curl gnupg lsb-release",
 
-      // Добавляем официальный репозиторий Docker
+      # Добавляем официальный репозиторий Docker
       "curl -4 -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
       "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
 
-      // Обновляем apt после добавления репозитория
+      # Обновляем apt после добавления репозитория
       "sudo apt-get update -o Acquire::ForceIPv4=true",
 
-      // Устанавливаем Docker и компоненты
+      # Устанавливаем Docker и дополнительные компоненты
       "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin",
 
-      // Обновляем secure_path в sudoers, чтобы docker был доступен при вызове sudo
-      "sudo sed -i 's|^Defaults\\s\\+secure_path=.*|Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"|' /etc/sudoers",
-
-      // Запускаем и включаем Docker
+      # Запускаем и включаем Docker
       "sudo systemctl enable docker",
       "sudo systemctl start docker",
 
-      // Добавляем пользователя ubuntu в группу docker
+      # Добавляем пользователя ubuntu в группу docker
       "sudo usermod -aG docker ubuntu",
 
-      // Проверяем установку Docker
+      # Проверяем установку Docker
       "docker --version"
     ]
   }
